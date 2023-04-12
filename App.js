c906b0db7ec3291eb08cf4f80459affea52f894e
Vicente-Ferrer <Vicenteferreira473@gmail.com>
2023-04-12 20:47:57 -0300

    Firts Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AppRoutes from "./src/routes/Index";
+import Home from "./src/Screens/Home/Index";
+
+const statusBarHeight = StatusBar.currentHeight
+  ? StatusBar.currentHeight + 18
+  : 45;
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: statusBarHeight }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
---
+++ src/Screens/CreateStudent/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const CreateStudent = () => {
+  return (
+    <View>
+      <Text>CreateStudent</Text>
+    </View>
+  );
+};
+
+export default CreateStudent;
